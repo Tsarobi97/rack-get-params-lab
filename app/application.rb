d31ae3b89f4aab115ed1373e50f3564f6e3f9bca
RobertObi <robobi97@googlemail.com>
2021-03-15 22:35:54 -0500

    commit tree caab65f6117d89f5f4feb689af2ff7206b8f6071

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,32 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+
+
+    if req.path.match(/cart/)
+      @@cart.each do |cart|
+      resp.write "#{cart}\n"
+    end 
+  elsif req.path.match(/add/)
+    add_term = req.params["q"]
+    resp.write handle_add(add_term)
+  end
+
+  
+  def handle_add(add_term)
+    if @@items.include?(add_term)
+      return "added #{add_term}"
+    else
+       "We don't have that item"
+    end
+  end
+
+
 
     if req.path.match(/items/)
       @@items.each do |item|
@@ -14,7 +36,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     else
-      resp.write "Path Not Found"
+      resp.write "Your cart is empty"
     end
 
     resp.finish
